7b4d29c45aae77da11e07874327fb064c3a86965
David Morales <david.morales.p@usach.cl>
2020-08-29 14:46:09 -0400

    Se implemento mas opciones en el menu

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,40 +16,49 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        System.out.println("### SIMULACIÓN DE GIT ###");
-        System.out.println("Escoja su opción:");
-        System.out.println("1. add");
-        System.out.println("2. commit");
-        System.out.println("3. pull");
-        System.out.println("4. push");
-        System.out.println("5. status");
-        System.out.println("6. Crear nuevo archivo");
-        System.out.println("INTRODUZCA SU OPCIÓN");
-        
-        Scanner in = new Scanner(System.in);
-        int opcion = in.nextInt();
-        if (opcion == 1){
-            System.out.println("gitAdd");            
-        }
-        else if (opcion == 2){
-            System.out.println("gitCommit");            
-        }
-        else if (opcion == 3){
-            System.out.println("gitPush");            
-        }
-        else if (opcion == 4){
-            System.out.println("gitPull");            
-        }
-        else if (opcion == 5){
-            System.out.println("gitStatus");            
-        }
-        else if (opcion == 6){
-          Archivo a = new Archivo();
-          a.crearArchivo(a);
-          System.out.println(a);            
-        }
-        else{
-            System.out.println("El valor no es válido.");
+        boolean menuSimulador = true;
+        boolean repositorioInicializado = false;
+        while (menuSimulador == true){
+            System.out.println("### SIMULACIÓN DE GIT ###");
+            System.out.println("Escoja su opción:");
+            System.out.println("1. add");
+            System.out.println("2. commit");
+            System.out.println("3. pull");
+            System.out.println("4. push");
+            System.out.println("5. status");
+            System.out.println("6. Crear nuevo archivo");
+            System.out.println("INTRODUZCA SU OPCIÓN");
+            
+            Scanner in = new Scanner(System.in);
+            int opcion = in.nextInt();
+            if (repositorioInicializado == true){
+                if (opcion == 1){
+                    System.out.println("gitAdd");            
+                }
+                else if (opcion == 2){
+                    System.out.println("gitCommit");            
+                }
+                else if (opcion == 3){
+                    System.out.println("gitPush");            
+                }
+                else if (opcion == 4){
+                    System.out.println("gitPull");            
+                }
+                else if (opcion == 5){
+                    System.out.println("gitStatus");            
+                }
+                else if (opcion == 6){
+                    Archivo a = new Archivo();
+                    a.crearArchivo(a);
+                    System.out.println(a);            
+                }
+                else{
+                    System.out.println("El valor no es válido.");
+                }
+            }
+            else{
+                System.out.println("Para realizar esta accion debe inicializar un nuevo repositorio.");
+            }
         }
     }
 }

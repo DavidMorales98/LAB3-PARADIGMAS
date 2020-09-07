206172120f2e8de1c44839030b7d2d8ea0fb73cf
David Morales <david.morales.p@usach.cl>
2020-09-07 00:58:15 -0300

    Requerimientos funcionales obligatorios terminados

---
+++ JavaApplication1/src/javaapplication1/LAB3.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author David
+ */
+public class LAB3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        boolean menuSimulador = true;
+        boolean repositorioInicializado = false;
+        
+        Scanner in = new Scanner(System.in);
+        while (repositorioInicializado == false){
+            System.out.println("¿Desea inicializar su repositorio?(1.-Si/2.-No)");
+        
+            int resp = in.nextInt();
+            if (resp == 1){
+                Repositorios git = new Repositorios();
+                git.init("LAB","David");
+                repositorioInicializado = true;
+                menuSimulador = true;
+            }
+            while (menuSimulador == true){
+                Workspace w = new Workspace();           
+                System.out.println("### SIMULACIÓN DE GIT ###");
+                System.out.println("Escoja su opción:");
+                System.out.println("1. add");
+                System.out.println("2. commit");
+                System.out.println("3. pull");
+                System.out.println("4. push");
+                System.out.println("5. status");
+                System.out.println("6. Crear nuevo archivo");
+                System.out.println("7. Salir");
+                System.out.println("INTRODUZCA SU OPCIÓN");    
+            
+                int opcion = in.nextInt();
+                if (repositorioInicializado == true){
+                    if (opcion == 1){
+                            System.out.println("gitAdd");
+                    }
+                    else if (opcion == 2){
+                            System.out.println("gitCommit");
+                    }
+                    else if (opcion == 3){
+                            System.out.println("gitPush");
+                    }
+                    else if (opcion == 4){
+                            System.out.println("gitPull");
+                    }
+                    else if (opcion == 5){
+                            System.out.println("gitStatus");
+                    }
+                    else if (opcion == 6){
+                            Archivo a = new Archivo();
+                            a.crearArchivo();
+                            a.toString();
+                                
+                    }
+                    else if (opcion == 7){
+                        System.out.println("Adiós, hasta pronto.");
+                        break;
+                                
+                    }
+                    else{
+                            System.out.println("El valor no es válido.");
+                    }
+                }
+                else{
+                    System.out.println("Para realizar esta accion debe inicializar un nuevo repositorio.");
+                    menuSimulador = false;
+                }
+            }
+        }
+    }    
+}
+
+
